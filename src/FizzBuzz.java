066f400ddd9b6cdefd77b5f708600d02a97137ee
rainychen <rainy.chen@mail.utoronto.ca>
2024-09-09 16:45:23 -0400

    changing from for loop to while loop

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -6,7 +6,8 @@ class FizzBuzz {
     public static void main(String[] args) {
 
         int i = 1;
-        while (i < 100; i++; {
+        while (i < 100) {
+            i++;
 
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
