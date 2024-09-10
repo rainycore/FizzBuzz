0cd6404667a807c9e0264dace8b19caa2a1e75fc
rainychen <rainy.chen@mail.utoronto.ca>
2024-09-10 15:00:26 -0400

    added helper function from FizzBuzz to Main

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,35 +7,33 @@ class FizzBuzz {
 
         int i = 1;
         while (i < 100) {
-            i = doFizzBuzz(i);
+            i++;
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                System.out.println("Fizz Buzz");
+
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+
+            }
         }
     }
 
-    private static int doFizzBuzz(int i) {
-        i++;
-
-        // Find out which numbers divide i.
-        boolean divisibleBy3 = i % 3 == 0;
-        boolean divisibleBy5 = i % 5 == 0;
-
-        // Print our appropriate result.
-        if (divisibleBy3 && divisibleBy5) {
-
-            System.out.println("Fizz Buzz");
-
-        } else if (divisibleBy3) {
-
-            System.out.println("Fizz");
-
-        } else if (divisibleBy5) {
-
-            System.out.println("Buzz");
-
-        } else {
-
-            System.out.println(i);
-
-        }
-        return i;
+    public static void doFizzBuzz(int i) {
     }
 }
---
+++ src/Main.java
@@ -0,0 +1,4 @@
+public static void main(String[] args) {
+    FizzBuzz.main(args);
+    FizzBuzz.doFizzBuzz(5);
+}
