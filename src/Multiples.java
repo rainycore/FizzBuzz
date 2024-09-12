f903c791183314af4759229bd3caa20636a95b2f
rainychen <rainy.chen@mail.utoronto.ca>
2024-09-12 14:45:40 -0400

    create Multiples.java

---
+++ src/Multiples.java
@@ -0,0 +1,19 @@
+class Multiples {
+    public static void main(String args[])
+    {
+        int x = 1;
+        int count = 0;
+
+        // Exit when x becomes greater than 1000
+        while (x < 1000) {
+            // check if it is divisible by 3 or 5
+            if (x % 3 == 0 || x % 5 == 0)
+            {count = count + x;}
+
+
+            // next iteration
+            x++;
+        }
+        System.out.println(count);
+    }
+}
